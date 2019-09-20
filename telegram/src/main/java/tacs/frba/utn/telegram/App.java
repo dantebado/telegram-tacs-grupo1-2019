712d4ca0fab03dd68c6f6d1d96c2a7a3f653fcdf
Dante Mateo Bado Sucamele <dantebado@gmail.com>
2019-09-20 14:13:59 -0300

    Initial maven setup

---
+++ telegram/src/main/java/tacs/frba/utn/telegram/App.java
@@ -0,0 +1,13 @@
+package tacs.frba.utn.telegram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
